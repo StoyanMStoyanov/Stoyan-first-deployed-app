cf8961c86cc34449b616194d14cc704c34771330
StoyanMStoyanov <stoyanovengineer@gmail.com>
2016-10-03 10:33:40 +0300

    App first init.

---
+++ index.js
@@ -0,0 +1,18 @@
+let http = require ('http');
+
+let port = process.env.PORT || 5000;
+
+let environment = process.env.NODE_ENV;
+
+http
+	.createServer((request, responce) => {
+		responce.writeHead(200);
+		responce.write('Hi Stoyan from your first real deploued app.\nEnvironment: ' + 
+			environment + '. Port: ' + port + '.');
+		responce.end();
+	})
+	.listen(port);
+
+	if (environment === 'development') {
+		console.log(`Server running on port: ${port}.`);
+	};
