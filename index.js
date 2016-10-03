6fd2701e3df53969e356a10ab158bc86010cd8bc
StoyanMStoyanov <stoyanovengineer@gmail.com>
2016-10-03 11:30:21 +0300

    Place ; at the end of line.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let http = require ('http')
+let http = require ('http');
 
-let port = process.env.PORT || 5000
-let environment = process.env.NODE_ENV
+let port = process.env.PORT || 5000;
+let environment = process.env.NODE_ENV;
 
 http
 	.createServer((req, res) => {
-		res.writeHead(200)
+		res.writeHead(200);
 		res.write('Hi Stoyan from your first real deploued app.\nEnvironment: ' + 
-			environment + '. Port: ' + port + '.')
-		res.end()
+			environment + '. Port: ' + port + '.');
+		res.end();
 	})
-	.listen(port)
+	.listen(port);
 
 	if (environment === 'development') {
-		console.log(`Server running on port: ${port}.`)
+		console.log(`Server running on port: ${port}.`);
 	}
