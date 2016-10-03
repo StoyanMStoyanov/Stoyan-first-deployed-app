8b5511069fba94a5ad5357b90a238a9bbec10e73
StoyanMStoyanov <stoyanovengineer@gmail.com>
2016-10-03 10:58:04 +0300

    Repair some errors.

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ let port = process.env.PORT || 5000;
 
 let environment = process.env.NODE_ENV;
 
-http
-	.createServer((request, responce) => {
+http.createServer((request, responce) => {
 		responce.writeHead(200);
 		responce.write('Hi Stoyan from your first real deploued app.\nEnvironment: ' + 
 			environment + '. Port: ' + port + '.');
 		responce.end();
-	})
-	.listen(port);
+	}).listen(port);
 
 	if (environment === 'development') {
 		console.log(`Server running on port: ${port}.`);
